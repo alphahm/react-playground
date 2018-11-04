489aabd8121605154c3430b8ed0a0cc246434b40
alphahm <alphahm@github>
2018-11-04 18:57:53 +0000

    a simple react playground using babel, browserify and lite-server

---
+++ src/Hello.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HelloMessage extends Component {
+    
+    render() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+}
+
+export default HelloMessage;
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import HelloMessage from './Hello.jsx';
+
+
+render(<HelloMessage name="World"/>, document.getElementById("root"));
